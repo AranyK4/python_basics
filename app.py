51a9d7f0035d659d9e8c6756654555903ee7537e
Arany Kincső Emese <kincsoarany2006@gmail.com>
2024-10-02 13:20:20 +0200

    learned about if statement.

---
+++ app.py
@@ -0,0 +1,12 @@
+a: bool = True
+b: bool = False
+
+# Truthy Falsy
+
+# Learn if statement
+""" if a:
+    print("a is True")
+elif b:
+    print("b is True")
+else:
+    print("neither a or b is True") """
