3d9b1993c529192916435e49d32e2b0465a278e1
Arany Kincső Emese <kincsoarany2006@gmail.com>
2024-10-09 13:51:39 +0200

    Classes

---
+++ class.py
@@ -0,0 +1,6 @@
+# User defined data types
+class wardrobe:
+    def __init__(self, color, shelf_count, height):
+        self.color = color
+        self.shelf = shelf_count
+        self.height = height
